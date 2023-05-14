efa1c98d04b4e6aae8ac7ad1f27ceae8e571e793
Mune <mune@mune.moe>
2023-05-14 17:45:12 +0700

    VCL: Fix fallback port for server

--- apps/vietnam-community-league/server.js
+++ apps/vietnam-community-league/server.js
@@ -1,7 +1,7 @@
 const spawn = require("node:child_process").spawn;
 const config = require("@vncommunityleague/app-config").default;
 
-const PORT = process.env.PORT || config.servers.vietnam_community_league.port || 4000;
+const PORT = process.env.PORT || config.servers.vietnam_community_league.port || 3000;
 
 const startServer = (command) => {
     spawn("npx", ["next", command, "-p", PORT], {
